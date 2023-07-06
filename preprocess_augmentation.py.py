9f67b71a06e93caa0206dd1fe6a605873111d485
AkashRamakrishnan <51354068+AkashRamakrishnan@users.noreply.github.com>
2023-07-06 15:58:08 +0200

    Create preprocess_augmentation.py.py

---
+++ preprocess_augmentation.py.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+import h5py
+import numpy as np
+import SimpleITK as sitk
+import numpy as np
+from scipy.ndimage import zoom
+import os
+from tqdm import tqdm
+from skimage.transform import resize
+import nibabel as nib
+
