00a2a60a0dd89fd3a95b184cc813a0fdc729e6fa
AkashRamakrishnan <51354068+AkashRamakrishnan@users.noreply.github.com>
2023-07-06 16:07:26 +0200

    Create loss.py

---
+++ loss.py
@@ -0,0 +1,101 @@
+import torch
+import numpy as np
+from scipy.ndimage import _ni_support
+from scipy.ndimage.morphology import distance_transform_edt, binary_erosion,\
+    generate_binary_structure
+from scipy.ndimage.measurements import label, find_objects
+from scipy.stats import pearsonr
+
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob)
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        # print("shape target:", target.shape)
+        target = self._one_hot_encoder(target)
+        # print("shape target after one hot:", target.shape)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict & target shape do not match'
+        class_wise_dice = []
+        loss = 0.0
+        # print(target[:, 0])
+        for i in range(1, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+
+def entropy_minmization(p):
+    y1 = -1*torch.sum(p*torch.log(p+1e-6), dim=1)
+    ent = torch.mean(y1)
+
+    return ent
+
+
+def entropy_map(p):
+    ent_map = -1*torch.sum(p * torch.log(p + 1e-6), dim=1,
+                           keepdim=True)
+    return ent_map
+
+
+def compute_kl_loss(p, q):
+    p_loss = F.kl_div(F.log_softmax(p, dim=-1),
+                      F.softmax(q, dim=-1), reduction='none')
+    q_loss = F.kl_div(F.log_softmax(q, dim=-1),
+                      F.softmax(p, dim=-1), reduction='none')
+
+    # Using function "sum" and "mean" are depending on your task
+    p_loss = p_loss.mean()
+    q_loss = q_loss.mean()
+
+    loss = (p_loss + q_loss) / 2
+    return loss
+
+def dc(result, reference):
+
+    result = numpy.atleast_1d(result.astype(numpy.bool))
+    # print("result:",result[:10])
+    reference = numpy.atleast_1d(reference.astype(numpy.bool))
+    # print("reference:", reference.shape, "result:", result.shape)
+    intersection = numpy.count_nonzero(result & reference)
+
+    size_i1 = numpy.count_nonzero(result)
+    size_i2 = numpy.count_nonzero(reference)
+
+    try:
+        dc = 2. * intersection / float(size_i1 + size_i2)
+    except ZeroDivisionError:
+        dc = 0.0
+
+    return dc
+
+def one_hot_encoders(input_tensor, num_classes):
+    tensor_list = []
+    for i in range(num_classes):
+        temp_prob = input_tensor == i * torch.ones_like(input_tensor)
+        tensor_list.append(temp_prob)
+    output_tensor = torch.cat(tensor_list, dim=1)
+    return output_tensor.float()
